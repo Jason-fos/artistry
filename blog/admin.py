9364955ef8d23be74f3debce038c29f9b945172a
Jason-fos <>
2023-08-08 19:49:37 +0000

    add django_summernote to installed apps in settings.py

--- blog/admin.py
+++ blog/admin.py
@@ -11,6 +11,19 @@ class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
+    def approve_post(self, request, queryset):
+        queryset.update(approved=True)
 
-admin.site.register(Comment)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
 admin.site.register(Category)
