478af12c37423d8a6ba41347afaed418406270e1
Jason-fos <>
2023-08-08 18:03:44 +0000

    add model for commenting on posts and model for post categories

--- blog/models.py
+++ blog/models.py
@@ -11,8 +11,8 @@ STATUS = (
 
 # model for posts
 class Post(models.Model):
-    title = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length=50, unique=True)
+    title = models.CharField(max_length=25, unique=True)
+    slug = models.SlugField(max_length=25, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
@@ -22,7 +22,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name="blog_likes", blank=True)
 
-    class = Meta:
+    class Meta:
         ordering = ["-created_on"]
 
     def __str__(self):
@@ -30,3 +30,31 @@ class Post(models.Model):
 
     def number_of_likes(self):
         return self.likes.count()
+
+
+# model for commenting on posts
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+
+# model for post categories
+class Category(models.Model):
+    title = models.CharField(max_length=25)
+    subtitle = models.CharField(max_length=25)
+    slug = models.SlugField()
+    thumbnail = CloudinaryField("image", default="placeholder")
+
+    def __str__(self):
+        return self.title
