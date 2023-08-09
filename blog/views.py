b2ebfe7ad6ecafdfaf3adef66911b5c7696d4c2a
Jason-fos <>
2023-08-09 17:33:07 +0000

    add homepage view to views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post, Category, Author
 
-# Create your views here.
+
+# homepage view
+class homepage(request):
+    categories = Category.objects.all()[0:3]
+    featured = Post.objects.filter(featured=True)
+    latest = Post.objects.order_by("-timestamp")[0:3]
+    context = {
+        "object_list": featured,
+        "latest": latest,
+        "categories": categories
+    }
+
+    return render(request, "index.html", context)
+# view to see all posts
+class all_posts(generic.ListView)
