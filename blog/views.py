be80b7a7ad00ed758d1f1bd0f48ee4b5e24aa1e2
Jason-fos <>
2023-08-09 17:47:21 +0000

    add base, index and all_posts.html files to templates and add boilerplate html to base, add style links to base.html

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,24 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post, Category, Author
 
 
 # homepage view
-class homepage(request):
+class homepage(View):
     categories = Category.objects.all()[0:3]
     featured = Post.objects.filter(featured=True)
     latest = Post.objects.order_by("-timestamp")[0:3]
+    template_name = "index.html"
     context = {
         "object_list": featured,
         "latest": latest,
         "categories": categories
     }
 
-    return render(request, "index.html", context)
+
 # view to see all posts
-class all_posts(generic.ListView)
+class all_posts(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "all_posts.html"
+    paginate_by = 6
